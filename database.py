c8d47e5abf8d043a9c21844e4ae2e0f0ffffa3a7
HectorVelez568 <hvelez74@hotmail.com>
2026-02-22 18:27:25 -0500

    fix: agregar email-validator

--- database.py
+++ database.py
@@ -3,15 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from typing import Generator
 
-# 🔥 TOMAR DESDE VARIABLE DE ENTORNO
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# ⚠️ FALLBACK SOLO PARA LOCAL
-if not DATABASE_URL:
-    DATABASE_URL = "postgresql+psycopg://glosas_user:Glosas_2025*@localhost:5432/trazabilidad_glosas"
-
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
